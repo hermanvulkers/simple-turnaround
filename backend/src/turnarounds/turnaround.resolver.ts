86f910dc933d6952df902809f5ece84a6b2ab1a1
Herman Vulkers <info@sonobe.nl>
2025-07-05 23:40:14 +0200

    chore: add query root type

--- backend/src/turnarounds/turnaround.resolver.ts
+++ backend/src/turnarounds/turnaround.resolver.ts
@@ -1,17 +1,22 @@
 import { Inject } from '@nestjs/common';
-import { Resolver, Subscription } from '@nestjs/graphql';
+import { Query, Resolver, Subscription } from '@nestjs/graphql';
 import { PubSub } from 'graphql-subscriptions';
 import { TurnaroundEvent } from './entities/turnaround-event.entity';
 
 @Resolver()
 export class TurnaroundResolver {
-  constructor(
-    @Inject('PUB_SUB')
-    private readonly pubSub: PubSub<{ TURNAROUND_UPDATED: TurnaroundEvent }>,
-  ) {}
+  @Query(() => String)
+  ping(): string {
+    return 'pong';
+  }
 
   @Subscription(() => TurnaroundEvent, { name: 'turnaroundUpdated' })
   turnaroundUpdated(): AsyncIterableIterator<TurnaroundEvent> {
     return this.pubSub.asyncIterableIterator('TURNAROUND_UPDATED');
   }
+
+  constructor(
+    @Inject('PUB_SUB')
+    private readonly pubSub: PubSub<{ TURNAROUND_UPDATED: TurnaroundEvent }>,
+  ) {}
 }
